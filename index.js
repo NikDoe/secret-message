f81297da89a36e11c109ae3357583203aa6269f3
NikDoe <nikd0e@yandex.ru>
2022-07-09 21:36:33 +0300

    feature(secret-msg):generating the url

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 document.querySelector('form').addEventListener('submit', event => {
 	event.preventDefault();
 	const input = document.querySelector('#message-input');
-	document.querySelector('#link-input').value = btoa(input.value);
+	const encrypted = btoa(input.value);
+	const linkInput = document.querySelector('#link-input');
+	linkInput.value = `${window.location}#${encrypted}`;
+	linkInput.select();
 });
